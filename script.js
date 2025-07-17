ec06fefd437d8907e9d67273a57748a37e546e65
ialeguilherme <ialeguilherme99@gmail.com>
2025-07-16 23:18:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // --- Configuração do Supabase ---
     // Substitua com suas próprias credenciais do Supabase
+    
     import { createClient } from '@supabase/supabase-js'
-    const supabaseUrl = 'SUA_URL_DO_SUPABASE'
-    const supabaseKey = 'SUA_CHAVE_ANONIMA'
+
+const supabaseUrl = 'SUA_URL_DO_SUPABASE'
+const supabaseKey = 'SUA_CHAVE_ANONIMA'
+
+const supabase = createClient(supabaseUrl, supabaseKey)
     const SUPABASE_URL = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmaG5naHlxbWR1aXlpY2ZxZHRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0NTA4NjUsImV4cCI6MjA2ODAyNjg2NX0._UKv8z0MIC96q4oMFU6vZkMCUUjolxf86LizMCaDtxo'; // Ex: https://abcdefghijklmn.supabase.co
     const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmaG5naHlxbWR1aXlpY2ZxZHRpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjQ1MDg2NSwiZXhwIjoyMDY4MDI2ODY1fQ.ABIfz3Gf29cUP_6Mq4_1VE5zndw5lYwXL6Eial-Q19E'; // Ex: eyJhbGciOiJIUzI1Ni...
 
